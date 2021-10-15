681da6797f57de130cfab277f54705827bdb6c8b
qaasima <qaasima@uni.coventry.ac.ul>
2021-10-15 15:02:11 +0100

    initialising a jokes bank using a dictionary then running a function to choose the jokes randomly with key and value pair

---
+++ jokegenerator.js
@@ -0,0 +1,27 @@
+jokes_bank = {
+    "How do you get a squirrel to like you?": "Act like a nut.",
+    
+    "What do you call two birds in love?" : "Tweethearts",
+    
+    "How does a scientist freshen her breath?" : "With experi-mints.",
+    
+    "How are false teeth like stars?" : "They come out at night.",
+    
+    "What building in your town has the most stories?" : "The public library.",
+    
+    "What’s worse than finding a worm in your apple?" : "Finding half a worm.",
+    
+    "What is a computer’s favorite snack?" : "Computer chips.",
+    
+    "What did one volcano say to the other?" : "I lava you.",
+    
+    "How do we know that the ocean is friendly?" : "It waves.",
+    
+    "What is a tornado’s favorite game to play?" : "Twister.",
+    
+    "How does the moon cut his hair?": "Eclipse it.",
+    
+    "How do you talk to a giant?": "Use big words."}
+;
+
+console.log(Object.keys(jokes_bank));
